0c71fc814bec6c7a4dd6370de0d823d11a6a241a
Jeff Gauthier <code@bogue.ca>
2019-09-10 22:43:55 -0400

    Basic 404

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../component/Layout';
+
+export default ({ pageContext: { news } }) => (
+  <Layout pageTitle="Page not found">
+    <div style={{
+      height: '60vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      }}><span>There is no spoon.</span></div>
+  </Layout>
+);
