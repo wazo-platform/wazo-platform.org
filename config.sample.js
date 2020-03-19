3e768d3f3112d2715a8c6a76e86b379677cb230d
Francis Chartrand <chartrandf@bonyweb.com>
2020-03-19 16:17:44 -0400

    Split algolia index keys from both websites

--- config.sample.js
+++ config.sample.js
@@ -3,5 +3,7 @@ module.exports = {
     appId: '',
     publicKey: '',
     apiKey: '',
+    indexDeveloper: `developer-wazo-io-${process.env.NODE_ENV}`,
+    indexPlatform: `wazo-platform-${process.env.NODE_ENV}`,
   }
 };
