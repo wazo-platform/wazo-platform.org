15521263d143057fb54ce5c89fcb33db73c4996b
Jeff Gauthier <code@bogue.ca>
2020-05-11 10:14:58 -0400

    one-pager tweaks

--- src/component/provisioning/vendors.js
+++ src/component/provisioning/vendors.js
@@ -7,46 +7,45 @@ const slugify = require('../../builder/slugify');
 const title = 'Provd Plugins';
 
 export default ({ pageContext: { plugins, images } }) => {
-  const [current, setCurrent] = useState(null);
+  const [currentVendor, setCurrentVendor] = useState(null);
+  const [currentDevice, setCurrentDevice] = useState(null);
 
-  const onClick = event => {
+  const onDeviceClick = event => {
     event.preventDefault();
-    console.log('click', event.target.id, event.target);
-    setCurrent(current === event.target.id ? null : event.target.id);
+    setCurrentDevice(currentDevice === event.target.id ? null : event.target.id);
   }
+
+  const onVendorClick = event => {
+    event.preventDefault();
+    setCurrentVendor(currentVendor === event.target.id ? null : event.target.id);
+  }
+
   return (
     <Layout pageTitle={title}>
       <div className="doc-wrapper provisioning-vendors">
         <div className="container">
           <div className="section-block">
-            <div className="row">
-              {Object.keys(plugins).map(vendor => (
-                <div className="vendor" key={vendor}>
-                  <div className="card">
-                    <a href={`/provisioning/${slugify(vendor)}`} className="card-header">{vendor}</a>
-                    <div className="body">
-                      <a href={`/provisioning/${slugify(vendor)}`}>
-                        <img src={`/provisioning/${slugify(vendor)}.png`} alt=""/>
-                      </a>
-                    </div>
-                  </div>
+            {Object.keys(plugins).map(vendor => (
+              <div className="vendor" key={vendor}>
+                <a className="title" href={`/provisioning/${slugify(vendor)}`} id={vendor} onClick={onVendorClick}>{vendor}</a>
+                {vendor === currentVendor && 
                   <div className="phones">
                     {Object.keys(plugins[vendor]).map(name => {
                       const id = `${slugify(vendor)}-${slugify(name)}`;
-                      return <div className={id === current ? 'selected' : ''} key={id} >
-                        <a href={`/provisioning/${slugify(vendor)}/${slugify(name)}`} onClick={onClick} id={id}>{name}</a>
-                        {id === current && <div className="slide">
+                      return <div className={id === currentDevice ? 'selected' : ''} key={id} >
+                        <a href={`/provisioning/${slugify(vendor)}/${slugify(name)}`} onClick={onDeviceClick} id={id}>{name}</a>
+                        {id === currentDevice && <div className="slide">
                           <div className="image">
+                            <div className="name">{name}</div>
                             {images[slugify(vendor)].indexOf(`${slugify(name)}.png`) !== -1 ? <img src={`/provisioning/${slugify(vendor)}-${slugify(name)}.png`} alt={`${slugify(vendor)}-${name}`}/> : <img src='/provisioning/img-placeholder.png' alt={`${slugify(vendor)}-${name}`} />}
                           </div>
                           <div className="content">{buildTable(plugins[vendor][name])}</div>
                     </div>}
                       </div>;
                     })}
-                  </div>
-                </div>
-              ))}
-            </div>
+                </div>}
+              </div>
+            ))}
           </div>
         </div>
       </div>
