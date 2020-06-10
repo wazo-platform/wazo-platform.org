586e66493b12cbb4c9d21f0adce0cdcd3b2e8723
Francis Chartrand <chartrandf@bonyweb.com>
2020-06-10 09:19:41 -0400

    Remove overview on developer site for management platform

--- src/component/dev/index.js
+++ src/component/dev/index.js
@@ -7,31 +7,28 @@ const ExternalOrInternalLink = ({ children, to, activeClassName, partiallyActive
   // Tailor the following test to your environment.
   // This example assumes that any internal link (intended for Gatsby)
   // will start with exactly one slash, and that anything else is external.
-  const internal = /^\/(?!\/)/.test(to)
+  const internal = /^\/(?!\/)/.test(to);
   // Use Link for internal links, and <a> for others
   if (internal) {
     return (
-      <Link
-        to={to}
-        activeClassName={activeClassName}
-        partiallyActive={partiallyActive}
-        {...other}
-      >
+      <Link to={to} activeClassName={activeClassName} partiallyActive={partiallyActive} {...other}>
         {children}
       </Link>
-    )
+    );
   }
   return (
     <a href={to} {...other}>
       {children}
     </a>
-  )
-}
+  );
+};
 
 export const Module = ({ moduleName, module }) => (
-  <div className={`item item-red col-lg-4 col-6`}>
+  <div id={moduleName} className={`item item-red col-lg-4 col-6`}>
     <div className="item-inner">
-      <ExternalOrInternalLink to={module.url || `/documentation/overview/${moduleName}.html`}>
+      <ExternalOrInternalLink
+        to={module.overview === false ? `#${moduleName}` : module.url || `/documentation/overview/${moduleName}.html`}
+      >
         <div className="icon-holder">
           <i className={`icon ${module.icon}`} />
         </div>
@@ -40,26 +37,30 @@ export const Module = ({ moduleName, module }) => (
 
       <p className="intro">{module.description}</p>
       {!module.url && (
-          <div className="list-group">
-              <Link to={`/documentation/overview/${moduleName}.html`} className="list-group-item">
-                Overview
-              </Link>
+        <div className="list-group">
+          {module.overview !== false && (
+            <Link to={`/documentation/overview/${moduleName}.html`} className="list-group-item">
+              Overview
+            </Link>
+          )}
 
-            {module.redocUrl && (
-                <Link to={`/documentation/api/${moduleName}.html`} className="list-group-item">
-                  API Reference
-                </Link>
-            )}
-            {module.redocUrl && (
-                <Link to={`/documentation/console/${moduleName}`} className="list-group-item">
-                  API Console
-                </Link>
-            )}
+          {module.redocUrl && (
+            <Link to={`/documentation/api/${moduleName}.html`} className="list-group-item">
+              API Reference
+            </Link>
+          )}
+          {module.redocUrl && (
+            <Link to={`/documentation/console/${moduleName}`} className="list-group-item">
+              API Console
+            </Link>
+          )}
 
-              <a href={`https://github.com/wazo-platform/${module.repository}`} className="list-group-item">
-                <i className="fab fa-github" /> {module.repository}
-              </a>
-          </div>
+          {module.repository && (
+            <a href={`https://github.com/wazo-platform/${module.repository}`} className="list-group-item">
+              <i className="fab fa-github" /> {module.repository}
+            </a>
+          )}
+        </div>
       )}
     </div>
   </div>
@@ -71,12 +72,12 @@ export default ({ pageContext: { sections } }) => (
       <title>Wazo project documentation for developers</title>
     </Helmet>
     <section className="cards-section text-center">
-      {sections.map(section => (
+      {sections.map((section) => (
         <div key={section.name}>
           <h3>{section.name}</h3>
           <div className="container">
             <div className="cards-wrapper row">
-              {Object.keys(section.modules).map(moduleName => (
+              {Object.keys(section.modules).map((moduleName) => (
                 <Module key={moduleName} moduleName={moduleName} module={section.modules[moduleName]} />
               ))}
             </div>
