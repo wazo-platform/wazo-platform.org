bdff0121127a58b83bbedb6c6980fb79be1098e7
Francois Blackburn <fblackburn@wazo.community>
2020-06-11 09:45:36 -0400

    fix corporate typo

--- src/component/documentation/helper.js
+++ src/component/documentation/helper.js
@@ -1,5 +1,5 @@
 export const sepcBaseUrl = 'https://openapi.wazo.community';
 
-export const getModuleSpecPrefix = module => module.coporate ? 'nestbox' : 'wazo-platform';
+export const getModuleSpecPrefix = module => module.corporate ? 'nestbox' : 'wazo-platform';
 
 export const getModuleSpecUrl = module => `${sepcBaseUrl}/${getModuleSpecPrefix(module)}/${module.repository}.yml`;
