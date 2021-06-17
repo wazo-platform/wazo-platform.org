e09d6bf10278351d63ebac2e4833674684c31b45
Francis Chartrand <chartrandf@bonyweb.com>
2021-06-17 08:53:03 -0400

    Improve styling for blog article

--- src/component/blog/article.js
+++ src/component/blog/article.js
@@ -7,10 +7,10 @@ export default ({ pageContext: { title, author, tags: tagsRaw, date: dateRaw, ca
   const date = new Date(dateRaw);
   const formattedDate = `${date.getDate()} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`;
   const tags = tagsRaw && tagsRaw.split(',');
-    
+
   return (
     <Layout pageTitle={title} className="article" section="blog">
-      <div className="container main" style={{ padding: '50px 15px 70px' }}>
+      <div className="container main">
         <div className="head">
           Posted on {formattedDate}  {" "}
           in <Link className="hilite" to="/blog" state={{ filter: { type: 'category', value: category }}}>{category}</Link> {" "}
@@ -18,9 +18,9 @@ export default ({ pageContext: { title, author, tags: tagsRaw, date: dateRaw, ca
           {tags && tags.length &&
             <span className="tags"> * Tagged with {tags.map(item => <Link key={item} className="hilite" to="/blog" state={{ filter: { type: 'tag', value: item }}}>{item}</Link>)}</span>}
         </div>
-        <ReactMarkdown source={body} />
+        <ReactMarkdown className="article--content" source={body} />
       </div>
-      
+
     </Layout>
   );
 }
