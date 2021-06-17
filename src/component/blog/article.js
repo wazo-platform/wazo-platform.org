fe12ff6b18595d431514dfed785c0a5641f5982f
Francis Chartrand <chartrandf@bonyweb.com>
2021-06-17 10:11:03 -0400

    Move article meta data to the bottom of the article

--- src/component/blog/article.js
+++ src/component/blog/article.js
@@ -11,13 +11,17 @@ export default ({ pageContext: { title, author, tags: tagsRaw, date: dateRaw, ca
   return (
     <Layout pageTitle={title} pageTitleDate={formattedDate} className="article" section="blog">
       <div className="container main">
-        <div className="head">
-          in <Link className="hilite" to="/blog" state={{ filter: { type: 'category', value: category }}}>{category}</Link> {" "}
-          by <Link className="hilite" to="/blog" state={{ filter: { type: 'author', value: author }}}>{author}</Link>  {" "}
-          {tags && tags.length &&
-            <span className="tags"> * Tagged with {tags.map(item => <Link key={item} className="hilite" to="/blog" state={{ filter: { type: 'tag', value: item }}}>{item}</Link>)}</span>}
+        <div className="article--content">
+          <ReactMarkdown source={body} />
+
+          <div className="article--content--footer">
+            <Link className="article--content--footer-author" to="/blog" state={{ filter: { type: 'author', value: author }}}>{author}</Link>
+            <p>Category: <Link to="/blog" state={{ filter: { type: 'category', value: category }}}>{category}</Link></p>
+            {tags && tags.length && (
+              <p className="tags">Tags: {tags.map(item => <Link key={item} to="/blog" state={{ filter: { type: 'tag', value: item }}}>{item}</Link>)}</p>
+            )}
+          </div>
         </div>
-        <ReactMarkdown className="article--content" source={body} />
       </div>
 
     </Layout>
