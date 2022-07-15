6e7135a60a8f6d08e28d938aea0ae6b5964af59f
Francois Blackburn <fblackburn@wazo.community>
2022-07-15 15:47:42 -0400

    fix InstantSearch component upgrade
    why: root is no more accepted as property

--- src/component/corporate/search.js
+++ src/component/corporate/search.js
@@ -15,8 +15,6 @@ import { algoliaIndexDeveloper as indexName} from '../../contants'
 
 const config = require('../../../config');
 
-const Root = props => <div className="main-search-box pt-3 pb-4 d-inline-block">{props.children}</div>;
-
 const list = css`
   position: absolute;
   width: 560px;
@@ -149,27 +147,28 @@ export default class Search extends Component {
     }
 
     return (
-      <InstantSearch
-        searchClient={this.searchClient}
-        indexName={indexName}
-        onSearchStateChange={this.updateState}
-        root={{ Root }}
-      >
-        <Input onFocus={this.focus} {...{ collapse, focused }} />
-        <HitsWrapper show={query.length > 0 && focused} hitsAsGrid={hitsAsGrid} ref={this.list}>
-          <Index indexName={indexName}>
-            <Results>
-              <Hits hitComponent={PageHit(this.disableHits)} />
-            </Results>
-          </Index>
-          <By>
-            Powered by{' '}
-            <a href="https://www.algolia.com">
-              <i className="fab fa-algolia" /> Algolia
-            </a>
-          </By>
-        </HitsWrapper>
-      </InstantSearch>
+      <div className="main-search-box pt-3 pb-4 d-inline-block">
+        <InstantSearch
+          searchClient={this.searchClient}
+          indexName={indexName}
+          onSearchStateChange={this.updateState}
+        >
+          <Input onFocus={this.focus} {...{ collapse, focused }} />
+          <HitsWrapper show={query.length > 0 && focused} hitsAsGrid={hitsAsGrid} ref={this.list}>
+            <Index indexName={indexName}>
+              <Results>
+                <Hits hitComponent={PageHit(this.disableHits)} />
+              </Results>
+            </Index>
+            <By>
+              Powered by{' '}
+              <a href="https://www.algolia.com">
+                <i className="fab fa-algolia" /> Algolia
+              </a>
+            </By>
+          </HitsWrapper>
+        </InstantSearch>
+      </div>
     );
   }
 }
