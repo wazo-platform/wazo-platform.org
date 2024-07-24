432a17ac795a8e27ef75200081a838dc87d27665
Jerome <jclaveau@wazo.io>
2024-07-24 16:52:03 -0400

    fix: asset path

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,13 +6,14 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src="/images/logo-square.svg" alt="Wazo Platform icon" width={196} height={196} />
+        <img src={useBaseUrl('/images/logo-square.svg')} alt="Wazo Platform icon" width={196} height={196} />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
