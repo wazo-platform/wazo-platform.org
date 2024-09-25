d0632f9f010e0a0bef3b6046cb9766872c442579
Francis Chartrand <chartrandf@users.noreply.github.com>
2024-09-25 13:49:45 -0400

    fix: apply suggestions from code review
    Co-authored-by: Jérôme Claveau <41510224+Romej@users.noreply.github.com>

--- scripts/beta-sync.sh
+++ scripts/beta-sync.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 # Goal: Temporary script that copy beta content to old website until migration will be done
-
 
 # 1. Sync release note & blog posts
 echo "🔃 Sync release note & blog posts from docusaurus\n"
@@ -7,5 +7,18 @@ rm -r content/blog || true
 mkdir content/blog
 cp website/release-notes/*.md website/blog/*.md content/blog/
 
-sed -i '' 's/tags: \[\(.*\)\]/tags: \1/g' content/blog/*.md # tags support
-sed -i '' 's/..\/static/https:\/\/beta.wazo-platform.org\//g' content/blog/*.md # image source
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    sed_cmd="sed -i ''"
+else
+    # Linux
+    sed_cmd="sed -i"
+fi
+
+# Modify author format
+$sed_cmd 's/authors: /author: /g' content/blog/*.md
+$sed_cmd 's/author: \[\(.*\)\]/author: \1/g' content/blog/*.md
+# Modify tags format
+$sed_cmd 's/tags: \[\(.*\)\]/tags: \1/g' content/blog/*.md
+# Update image source URLs
+$sed_cmd 's|../static|https://beta.wazo-platform.org|g' content/blog/*.md
